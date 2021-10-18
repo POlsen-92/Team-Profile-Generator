1504052ca64a20e0a09ac3e41771de45b3fa6996
FemBot 9000 <paige.el.olsen@gmail.com>
2021-10-17 22:04:32 -0700

    fixed up style of html with bootstrap

--- Develop/util/generateHtml.js
+++ Develop/util/generateHtml.js
@@ -4,8 +4,8 @@ const generateTeam = team => {
     // create the manager html
     const generateManager = manager => {
         return `
-        <div class="card employee-card">
-        <div class="card-header">
+        <div class="card employee-card col-4 mx-4 my-2">
+        <div class="card-header bg-dark text-white">
             <h2 class="card-title">${manager.getName()}</h2>
             <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
         </div>
@@ -23,8 +23,8 @@ const generateTeam = team => {
     // create the html for engineers
     const generateEngineer = engineer => {
         return `
-        <div class="card employee-card">
-    <div class="card-header">
+        <div class="card employee-card col-4 mx-4 my-2">
+    <div class="card-header bg-info text-white">
         <h2 class="card-title">${engineer.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
     </div>
@@ -42,8 +42,8 @@ const generateTeam = team => {
     // create the html for interns
     const generateIntern = intern => {
         return `
-        <div class="card employee-card">
-    <div class="card-header">
+        <div class="card employee-card col-4 mx-4 my-2">
+    <div class="card-header bg-success text-white">
         <h2 class="card-title">${intern.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
     </div>
@@ -97,10 +97,10 @@ module.exports = team => {
     <script src="https://kit.fontawesome.com/c502137733.js"></script>
 </head>
 
-<body>
+<body class="bg-light">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading">
+            <div class="col-12 jumbotron mb-3 team-heading bg-primary text-white">
                 <h1 class="text-center">My Team</h1>
             </div>
         </div>
